e171eef3e33bcb4ec1d800e399a12b2745cf5553
Арина <78840926+ar1shka@users.noreply.github.com>
2021-03-29 19:19:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,49 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+int ff = 0;
+for (int i = 0; i < len-1; i++) {
+for (int j = i+1; j <= len; j++) {
+if (arr[i] + arr[j] == value) ff=ff+1 ;
 }
-
+}
+return ff;
+}
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+int ff = 0, l = 0;
+for (int i = 0; i < len; i++) {
+if (arr[i] >= value)
+l = i;
+}
+for (int i = l; i >= 0; i--) {
+for (int j = 0; j < i; j++) {
+if (arr[i] + arr[j] == value)
+ff++;
+}
+}
+return ff;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+int count = 0, left = 0, right = len - 1;
+int ff = 0, l = 0;
+for (int i = 0; i < len; i++) {
+if (arr[i] >= value)
+l = i;
+}
+for (int i = 0; i < l; i++) {
+left = i + 1, right = l;
+while (left < right) {
+int m = (left + right) / 2;
+if (arr[m] < (value - arr[i]))
+left = m + 1;
+else
+right = m;
+}
+while (arr[left] == (value - arr[i])) {
+ff++;
+left++;
+}
+}
+return ff;
 }
