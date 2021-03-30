cef6c909d1c1a900ea168aff1e83735ffc07884f
Арина <78840926+ar1shka@users.noreply.github.com>
2021-03-30 18:45:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,15 +1,15 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
 int ff = 0;
-for (int i = 0; i < len-1; i++) {
-for (int j = i+1; j <= len; j++) {
-if (arr[i] + arr[j] == value)
-ff++;
+for (int i = 0; i < len - 1; i++) {
+for (int j = i + 1; j <= len; j++) {
+if (arr[i] + arr[j] == value) ff = ff + 1;
 }
 }
 return ff;
 }
-int countPairs2(int* arr, int len, int value) {
+int countPairs2(int* arr, int len, int value)
+{
 int ff = 0, l = 0;
 for (int i = 0; i < len; i++) {
 if (arr[i] >= value) {
@@ -26,27 +26,32 @@ ff++;
 return ff;
 }
 int countPairs3(int* arr, int len, int value) {
-int count = 0, left = 0, right = len - 1;
-int ff = 0, l = 0;
-for (int i = 0; i < len; i++) {
-if (arr[i] >= value) {
-l = i;
-break;
-}
-}
-for (int i = 0; i < l; i++) {
-left = i + 1, right = l;
-while (left < right) {
-int m = (left + right) / 2;
-if (arr[m] < (value - arr[i]))
-left = m + 1;
+int ff = 0;
+int r = len - 1;
+int l = 0;
+while (l < r - 1) {
+int m = (l + r) / 2;
+if (arr[m] <= value)
+l = m;
 else
-right = m;
+r = m;
 }
-while (arr[left] == (value - arr[i])) {
-ff++;
-left++;
+len = r - 1;
+for (int i = 0; i < len; i++) {
+l = i + 1, r = len - 1;
+int k = 0;
+while (l < r) {
+int m = (l + r) / 2;
+if (arr[m] < (value - arr[i]))
+l = m + 1;
+else
+r = m;
 }
+while (arr[l] == (value - arr[i])) {
+k++;
+l++;
+}
+ff = ff+k;
 }
 return ff;
 }
